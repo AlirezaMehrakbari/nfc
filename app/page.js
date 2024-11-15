22bf445f2703f8d06185e58b786580918e5f2923
AlirezaMehrakbari <alirezamehrakbari@gmail.com>
2024-11-15 16:07:21 +0330

    first commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,243 @@
-import Image from 'next/image'
+'use client'
+import React, {useState} from "react";
+import "daisyui/dist/full.css";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+const NFCApp = () => {
+    const [message, setMessage] = useState("");
+    const [method, setMethod] = useState("");
+    const [nfcData, setNfcData] = useState(null);
+    const [methodModal, setMethodModal] = useState(false);
+    const [scanModal, setScanModal] = useState(false);
+    const [successWrite, setSuccessWrite] = useState(false)
+
+    const handleScan = async () => {
+        if ("NDEFReader" in window) {
+            try {
+                setScanModal(true); // نمایش مودال هنگام اسکن
+                const ndef = new window.NDEFReader();
+                await ndef.scan();
+                console.log("Scan started successfully.");
+
+                ndef.onreading = (event) => {
+                    const {serialNumber, message} = event;
+                    console.log("NFC Tag Serial Number:", serialNumber);
+                    for (const record of message.records) {
+                        if (record.recordType === "text") {
+                            const textDecoder = new TextDecoder(record.encoding);
+                            const data = textDecoder.decode(record.data);
+                            setNfcData(data);
+                            console.log("NFC Tag Data:", data);
+                        } else if (record.recordType === "url") {
+                            const url = new TextDecoder().decode(record.data);
+                            setNfcData(url);
+                            console.log("URL Read from NFC Tag:", url);
+                        } else if (record.recordType === "email") {
+                            const email = new TextDecoder().decode(record.data);
+                            setNfcData(email);
+                            console.log("Email Read from NFC Tag:", email);
+                        }
+                    }
+                };
+
+                ndef.onreadingerror = () => {
+                    console.error("Cannot read data from NFC tag.");
+                };
+            } catch (error) {
+                console.error("Error during NFC scan:", error);
+            }
+        }
+    };
+
+    const handleWrite = async () => {
+        if ("NDEFReader" in window) {
+            try {
+                const ndef = new window.NDEFReader();
+                let dataToWrite = "";
+
+                if (method === "url") {
+                    dataToWrite = message;
+                } else if (method === "text") {
+                    dataToWrite = message;
+                } else {
+                    alert("Please choose a method (URL, or Text) first.");
+                    return;
+                }
+
+                await ndef.write({
+                    records: [{recordType: method, data: dataToWrite}],
+                });
+                console.log(`Message written to NFC tag: ${dataToWrite}`);
+                setSuccessWrite(true)
+                setMessage('')
+            } catch (error) {
+                console.error("Error during NFC write:", error);
+            }
+        } else {
+            alert("NFC is not supported on this device.");
+        }
+    };
+
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+            <h1 className="text-3xl font-bold mb-6 text-blue-600">NFC Reader/Writer</h1>
+
+            <div className="w-full max-w-md p-6 bg-white shadow-md rounded-md">
+                {/* دکمه شروع رایت */}
+                <button
+                    onClick={() => setMethodModal(true)}
+                    className="w-full py-2 bg-green-500 text-white rounded-md hover:bg-green-600 transition mb-4"
+                >
+                    Select Method Write
+                </button>
+
+                {/* انتخاب متد رایت */}
+                {methodModal && (
+                    <div className="modal modal-open">
+                        <div className="modal-box">
+                            <h2 className="text-xl font-bold">Choose Write Method</h2>
+                            <div className="mt-4">
+                                <button
+                                    onClick={() => {
+                                        setMethod("url")
+                                        setMethodModal(false)
+                                        set
+                                    }}
+                                    className="w-full py-2 bg-blue-500 text-white rounded-md mb-4"
+                                >
+                                    URL
+                                </button>
+                                <button
+                                    onClick={() => {
+                                        setMethod("text")
+                                        setMethodModal(false)
+                                    }}
+                                    className="w-full py-2 bg-blue-500 text-white rounded-md mb-4"
+                                >
+                                    Text
+                                </button>
+                            </div>
+                            <div className="modal-action">
+                                <button
+                                    onClick={() => setMethodModal(false)}
+                                    className="btn btn-primary"
+                                >
+                                    Close
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                )}
+
+                {/* نمایش ورودی مناسب بر اساس انتخاب متد */}
+                {method && (
+                    <div>
+                        <input
+                            type="text"
+                            placeholder={
+                                method === "email"
+                                    ? "Enter Email"
+                                    : method === "url"
+                                        ? "Enter URL"
+                                        : "Enter Text"
+                            }
+                            value={message}
+                            onChange={(e) => setMessage(e.target.value)}
+                            className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 mb-4"
+                        />
+                    </div>
+                )}
+
+                {/* دکمه شروع اسکن */}
+                <button
+                    onClick={handleScan}
+                    className="w-full py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition mb-4"
+                >
+                    Start NFC Scan
+                </button>
+
+                {/* مودال برای رید */}
+                {scanModal &&
+                    <div className="fixed inset-0 flex items-center justify-center bg-gray-600 bg-opacity-50 z-50">
+                        <div className="modal modal-open">
+                            <div className="modal-box">
+                                <h2 className="text-xl font-bold">Reading NFC Tag...</h2>
+                                <div className="flex justify-center items-center mt-4">
+                                    <div className="spinner-border animate-spin text-3xl text-blue-600"></div>
+                                </div>
+                                <div className="mt-4">
+
+                                    {nfcData ? (
+                                        <p className="text-lg font-semibold">Data: {nfcData}</p>
+                                    ) : (
+                                        <div className={'flex flex-col'}>
+                                            <p>Scanning...</p>
+                                            <div className={'flex items-center justify-center'}>
+                                                <span className="loading loading-dots loading-lg "></span>
+                                            </div>
+                                        </div>
+
+                                    )}
+                                </div>
+                                <div className="modal-action">
+                                    <button
+                                        onClick={() => {
+                                            setScanModal(false)
+                                            setNfcData(null)
+                                        }}
+                                        className="btn btn-primary"
+                                    >
+                                        Close
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                }
+
+
+
+                <button
+                    onClick={handleWrite}
+                    className="w-full py-2 bg-green-500 text-white rounded-md hover:bg-green-600 transition"
+                >
+                    Write to NFC
+                </button>
+            </div>
+            {
+                successWrite &&
+                <div>
+                    <div className="fixed inset-0 flex items-center justify-center bg-gray-600 bg-opacity-50 z-50">
+                        <div className="modal modal-open">
+                            <div className="modal-box">
+                                <div role="alert" className="alert alert-success mt-4">
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="#FFFF"
+                                        viewBox="0 0 24 24"
+                                        className="stroke-info h-6 w-6 shrink-0 ">
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth="2"
+                                            d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                                    </svg>
+                                    <span className={'text-white'}>Write Successful!</span>
+                                </div>
+                                <div className="modal-action">
+                                    <button
+                                        onClick={() => setSuccessWrite(false)}
+                                        className="btn btn-primary"
+                                    >
+                                        Close
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            }
         </div>
-      </div>
+    );
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default NFCApp;
